ae575e9c47bb9dbcd9566a57dd3eb5b717131fb4
Cyclmobile <95096495+Cyclmobile@users.noreply.github.com>
2026-04-28 12:17:37 +0200

    Add files via upload

---
+++ js/site-firebase.js
@@ -0,0 +1,87 @@
+function _0xd2ee(_0x32c501, _0x53a48d) {
+  _0x32c501 = _0x32c501 - 0xef;
+  const _0x25c786 = _0x25c7();
+  let _0xd2ee48 = _0x25c786[_0x32c501];
+  return _0xd2ee48;
+}
+function _0x25c7() {
+  const _0x5949b5 = [
+    "1402FnqqRr",
+    "https://cycl-77b6c.firebaseio.com",
+    "7281hGPevE",
+    "firestore",
+    "36NPHiLQ",
+    "firebase",
+    "176335XWpiXT",
+    "1:139900263133:web:9c72ae1fd6569fe6a1d934",
+    "AIzaSyDTR4_vw3x8pTxIAiS8Y0-3T4APCwotpyg",
+    "length",
+    "1593LXQxkQ",
+    "G-VPV104NQQW",
+    "cycl-77b6c",
+    "11zIupXM",
+    "10301436kgeioO",
+    "cycl-77b6c.firebaseapp.com",
+    "638394PemOUa",
+    "2420iZJFHq",
+    "apps",
+    "7320PtIwys",
+    "getCyclFirebase",
+    "initializeApp",
+    "14qGSalo",
+    "381980GKwkes",
+  ];
+  _0x25c7 = function () {
+    return _0x5949b5;
+  };
+  return _0x25c7();
+}
+((function (_0x4b4289, _0x1abbe1) {
+  const _0x14e471 = _0xd2ee,
+    _0x1fda98 = _0x4b4289();
+  while (!![]) {
+    try {
+      const _0x403fef =
+        -parseInt(_0x14e471(0xf5)) / 0x1 +
+        (parseInt(_0x14e471(0xf6)) / 0x2) *
+          (-parseInt(_0x14e471(0x100)) / 0x3) +
+        (parseInt(_0x14e471(0xfa)) / 0x4) * (-parseInt(_0x14e471(0xfc)) / 0x5) +
+        (-parseInt(_0x14e471(0x106)) / 0x6) *
+          (-parseInt(_0x14e471(0xf4)) / 0x7) +
+        parseInt(_0x14e471(0xf1)) / 0x8 +
+        (parseInt(_0x14e471(0xf8)) / 0x9) * (parseInt(_0x14e471(0xef)) / 0xa) +
+        (-parseInt(_0x14e471(0x103)) / 0xb) *
+          (-parseInt(_0x14e471(0x104)) / 0xc);
+      if (_0x403fef === _0x1abbe1) break;
+      else _0x1fda98["push"](_0x1fda98["shift"]());
+    } catch (_0x3f8d4c) {
+      _0x1fda98["push"](_0x1fda98["shift"]());
+    }
+  }
+})(_0x25c7, 0x2feea),
+  (function () {
+    const _0x45bb7c = _0xd2ee,
+      _0x1be12a = {
+        apiKey: _0x45bb7c(0xfe),
+        authDomain: _0x45bb7c(0x105),
+        databaseURL: _0x45bb7c(0xf7),
+        projectId: _0x45bb7c(0x102),
+        storageBucket: "cycl-77b6c.appspot.com",
+        messagingSenderId: "139900263133",
+        appId: _0x45bb7c(0xfd),
+        measurementId: _0x45bb7c(0x101),
+      };
+    window[_0x45bb7c(0xf2)] = function _0x4d335e() {
+      const _0x18fde8 = _0x45bb7c;
+      if (!window[_0x18fde8(0xfb)])
+        throw new Error("Firebase\x20SDK\x20is\x20not\x20loaded");
+      return (
+        !window["firebase"][_0x18fde8(0xf0)][_0x18fde8(0xff)] &&
+          window[_0x18fde8(0xfb)][_0x18fde8(0xf3)](_0x1be12a),
+        {
+          firebase: window[_0x18fde8(0xfb)],
+          db: window[_0x18fde8(0xfb)][_0x18fde8(0xf9)](),
+        }
+      );
+    };
+  })());
